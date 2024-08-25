575af8a8866ae5de7ead0e5a9169d709c1b075be
Priyanshu Satapathy <128492679+Chiku91@users.noreply.github.com>
2024-08-26 02:04:14 +0530

    Add files via upload

---
+++ WeatherApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Delhi",
+        feelslike: 24.84,
+        temp: 25.05,
+        tempMin: 25.05,
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze",
+     });
+
+     let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+     }
+    return(
+        <div style={{textAlign: "center"}}>
+            <h2>Weather App Designed By Priyanshu</h2>
+            <SearchBox updateInfo={updateInfo} />
+            <InfoBox info={weatherInfo}/>
+        </div>
+    )
+}
+
